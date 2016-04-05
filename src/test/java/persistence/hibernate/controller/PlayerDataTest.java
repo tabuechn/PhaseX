c33b611bf11af3a9065e1d91258abee76b466176
tabuechn <tabuechn@htwg-konstanz.de>
2016-04-05 13:43:17 +0200

    hibernate player Object with names

---
+++ src/test/java/persistence/hibernate/controller/PlayerDataTest.java
@@ -0,0 +1,34 @@
+package persistence.hibernate.controller;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tabuechn on 05.04.2016.
+ */
+public class PlayerDataTest {
+
+
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testSetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayerName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayerName() throws Exception {
+
+    }
+}
