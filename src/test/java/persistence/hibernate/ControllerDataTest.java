08ff6b342b3d6b59252ae815890fa1d0184b6056
tabuechn <tabuechn@htwg-konstanz.de>
2016-04-19 10:17:59 +0200

    added ControllerData Test

---
+++ src/test/java/persistence/hibernate/ControllerDataTest.java
@@ -0,0 +1,179 @@
+package persistence.hibernate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tabuechn on 19.04.2016.
+ */
+public class ControllerDataTest {
+
+    private ControllerData controllerData;
+
+
+    @Test
+    public void testGetStringID() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStringID() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer2() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer2() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer1() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer1() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer1Pile() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer1Pile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer2Pile() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer2Pile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer1PhaseString() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer1PhaseString() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer1PhaseString1() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer2PhaseString() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer2PhaseString() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPlayer2PhaseString1() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDiscardPile() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDiscardPile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDrawPile() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDrawPile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRoundState() throws Exception {
+
+    }
+
+    @Test
+    public void testSetRoundState() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStatusMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStatusMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStack1() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStack1() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStack2() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStack2() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStack3() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStack3() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStack4() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStack4() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStack() throws Exception {
+
+    }
+}
