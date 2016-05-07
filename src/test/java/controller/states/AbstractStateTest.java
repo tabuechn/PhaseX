7f86b0f2db0b78d40aaab143b0fa8f2df19098c7
konraifen88 <konraifen88@googlemail.com>
2016-05-07 09:36:39 +0200

    adding some unit tests

--- src/test/java/controller/states/AbstractStateTest.java
+++ src/test/java/controller/states/AbstractStateTest.java
@@ -1,37 +1,72 @@
 package controller.states;
 
-import controller.states.impl.DrawPhase;
-import controller.states.impl.StartPhase;
+import controller.states.impl.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Tarek on 29.09.2015. Be grateful for this superior Code!
  */
 public class AbstractStateTest {
 
-    private AbstractState abstractTest;
+    private static final String UNKNOWN_PHASE = "supaDupaPhaseName";
+    private AbstractState testee;
 
     @Before
     public void setUp() throws Exception {
-        abstractTest = new StartPhase();
+        testee = new StartPhase();
     }
 
     @Test
     public void testAllButStart() {
-        abstractTest.addToFinishedPhase(null, null, null);
-        abstractTest.discard(null, null);
-        abstractTest.drawHidden(null);
-        abstractTest.drawOpen(null);
-        abstractTest.playPhase(null, null);
-        assertEquals(abstractTest.toString(), "StartPhase");
+        testee.addToFinishedPhase(null, null, null);
+        testee.discard(null, null);
+        testee.drawHidden(null);
+        testee.drawOpen(null);
+        testee.playPhase(null, null);
+        assertEquals(testee.toString(), "StartPhase");
     }
 
     @Test
     public void testStart() {
-        abstractTest = new DrawPhase();
-        abstractTest.start(null);
+        testee = new DrawPhase();
+        testee.start(null);
+    }
+
+    @Test
+    public void startPhaseAsStringShouldReturnNewStartPhase() {
+        assertTrue(AbstractState.getStateFromString(AbstractState.START_PHASE) instanceof StartPhase);
+    }
+
+    @Test
+    public void drawPhaseAsStringShouldReturnNewDrawPhase() {
+        assertTrue(AbstractState.getStateFromString(AbstractState.DRAW_PHASE) instanceof DrawPhase);
+
+    }
+
+    @Test
+    public void endPhaseAsStringShouldReturnNewEndPhase() {
+        assertTrue(AbstractState.getStateFromString(AbstractState.END_PHASE) instanceof EndPhase);
+
+    }
+
+    @Test
+    public void playerTurnNotFinishedAsStringShouldReturnNewPlayerTurnNotFinishedPhase() {
+        assertTrue(AbstractState.getStateFromString(AbstractState.PLAYER_TURN_NOT_FINISHED) instanceof PlayerTurnNotFinished);
+
+    }
+
+    @Test
+    public void playerTurnFinishedPhaseAsStringShouldReturnNewPlayerTurnFinishedPhase() {
+        assertTrue(AbstractState.getStateFromString(AbstractState.PLAYER_TURN_FINISED) instanceof PlayerTurnFinished);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void unknownStringShouldThrowAnException() {
+        AbstractState.getStateFromString(UNKNOWN_PHASE);
     }
 }
