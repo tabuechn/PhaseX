ee99d0d78cef0eb019ddd875546501e94425dcb8
daschwin <daschwin@htwg-konstanz.de>
2017-06-20 13:30:25 +0200

    Add files via upload
    Extra Klassen für verschiedene arten von Phasen.
    Phases wird vom Generator erzeugt.

---
+++ src/main/java/model/phase/impl/PhaseColor.java
@@ -0,0 +1,64 @@
+package model.phase.impl;
+
+import model.deck.IDeckOfCards;
+import model.phase.DeckNotFitException;
+import model.phase.IPhase;
+import model.phase.IPhaseChecker;
+import model.phase.checker.ColorChecker;
+import model.stack.ICardStack;
+import model.stack.impl.ColorStack;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Tarek on 24.09.2015. Be grateful for this superior Code!
+ * <p>
+ * edited: Konraifen88
+ * date: 30.09.2015
+ * merged phase checker and getter
+ */
+public class PhaseColor implements IPhase {
+
+    public static final int PHASE_NUMBER = 3;
+    public static final int COLOR_SIZE = 6;
+    private static final String DESCRIPTION_PHASE_3 = "six cards of one color";
+    private IPhaseChecker phaseChecker;
+
+    public PhaseColor() {
+        phaseChecker = new ColorChecker(COLOR_SIZE);
+    }
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION_PHASE_3;
+    }
+
+    @Override
+    public List<ICardStack> splitAndCheckPhase(IDeckOfCards phase) throws DeckNotFitException {
+        if (phaseChecker.check(phase)) {
+            return Collections.singletonList(new ColorStack(phase));
+        }
+        throw new DeckNotFitException();
+    }
+
+    @Override
+    public IPhase getNextPhase() {
+        return new Phase4();
+    }
+
+    @Override
+    public int getPhaseNumber() {
+        return PHASE_NUMBER;
+    }
+
+    @Override
+    public boolean isNumberPhase() {
+        return phaseChecker instanceof ColorChecker;
+    }
+
+    @Override
+    public String toString() {
+        return "Phase3";
+    }
+}
---
+++ src/main/java/model/phase/impl/Phases.java
@@ -0,0 +1,82 @@
+package model.phase.impl;
+
+import model.card.impl.CardValueComparator;
+import model.deck.IDeckOfCards;
+import model.phase.DeckNotFitException;
+import model.phase.IPhase;
+import model.phase.IPhaseChecker;
+import model.phase.IPhaseSplitter;
+import model.phase.checker.ColorChecker;
+import model.phase.checker.ValueChecker;
+import model.phase.splitter.DeckSplitter;
+import model.stack.ICardStack;
+import model.stack.impl.PairStack;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Tarek on 24.09.2015. Be grateful for this superior Code!
+ *
+ * edited: Konraifen88
+ * date: 30.09.2015
+ * merged phase checker and getter
+ */
+public class Phases implements IPhase {
+
+    public static final int PHASE_NUMBER = 4;
+    private static final String DESCRIPTION_PHASE = "number quadruple and number pair";
+    Boolean isNumberPhase = false;
+
+    public Phases() {
+        //empty
+    }
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION_PHASE;
+    }
+
+    @Override
+    public List<ICardStack> splitAndCheckPhase(IDeckOfCards phase) throws DeckNotFitException {
+        String phaseType = null;
+        String[] numbersType = null;
+        List<ICardStack> pairStackList = Collections.emptyList();
+
+        if(phaseType.equals("NUMBERS")) {
+            isNumberPhase = true;
+            PhaseNumber phaseNumber = new PhaseNumber();
+            return phaseNumber.splitAndCheckPhase(phase, numbersType);
+        }
+        if(phaseType.equals("COLORS")) {
+            PhaseColor phaseNumber = new PhaseColor();
+            return phaseNumber.splitAndCheckPhase(phase);
+        }
+        if(phaseType.equals("STREET")) {
+            PhaseStreet phaseNumber = new PhaseStreet();
+            return phaseNumber.splitAndCheckPhase(phase);
+        }
+        throw new DeckNotFitException();
+    }
+
+    @Override
+    public IPhase getNextPhase() {
+        return new Phase5();
+    }
+
+    @Override
+    public int getPhaseNumber() {
+        return PHASE_NUMBER;
+    }
+
+    @Override
+    public boolean isNumberPhase() {
+        return isNumberPhase;
+    }
+
+    @Override
+    public String toString() {
+        return "Phase4";
+    }
+}
